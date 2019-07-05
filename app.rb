698b56e4b44d05a92f3e3c7451129ee7565bff6c
userb <userb@mail.local>
2019-07-05 21:04:41 +0900

    ファイル一覧を表示できるように修正

--- app.rb
+++ app.rb
@@ -5,14 +5,19 @@ require 'kramdown'
 Encoding.default_external = 'utf-8'
 
 get '/' do
-    'this is toppage'
+    begin
+        @files = Dir.glob('files/*')
+        erb :index
+    rescue => e
+        e.message
+    end
 end
 
 get '/files/:name' do
     begin
         File.open "files/#{params[:name]}" do |file|
             @md = file.read
-            erb :index
+            erb :content
         end
     rescue => e
         e.message
